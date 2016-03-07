6268831182c1b27cf2ee26f40adbd0e62c60bc23
Benjamin Borbe <bborbe@rocketnews.de>
2016-03-07 20:45:59 +0100

    add worker2

--- ssl/generate_keys.sh
+++ ssl/generate_keys.sh
@@ -3,34 +3,36 @@
 # https://coreos.com/kubernetes/docs/latest/openssl.html
 
 # CA Key
-
 openssl genrsa -out ca-key.pem 2048
 openssl req -x509 -new -nodes -key ca-key.pem -days 10000 -out ca.pem -subj "/CN=kube-ca"
 
 # Master Key
-
 openssl genrsa -out apiserver-key.pem 2048
 openssl req -new -key apiserver-key.pem -out apiserver.csr -subj "/CN=kube-apiserver" -config openssl.cnf
 openssl x509 -req -in apiserver.csr -CA ca.pem -CAkey ca-key.pem -CAcreateserial -out apiserver.pem -days 365 -extensions v3_req -extfile openssl.cnf
 
-# Worker Key 1
-
-WORKER_FQDN = 192.168.178.137
+# Worker 0 Key
+WORKER_FQDN = "worker0"
 WORKER_IP = 192.168.178.137
 openssl genrsa -out ${WORKER_FQDN}-worker-key.pem 2048
 WORKER_IP=${WORKER_IP} openssl req -new -key ${WORKER_FQDN}-worker-key.pem -out ${WORKER_FQDN}-worker.csr -subj "/CN=${WORKER_FQDN}" -config worker-openssl.cnf
 WORKER_IP=${WORKER_IP} openssl x509 -req -in ${WORKER_FQDN}-worker.csr -CA ca.pem -CAkey ca-key.pem -CAcreateserial -out ${WORKER_FQDN}-worker.pem -days 365 -extensions v3_req -extfile worker-openssl.cnf
 
-# Worker Key 2
-
-WORKER_FQDN = 192.168.178.138
+# Worker 1 Key
+WORKER_FQDN = "worker1"
 WORKER_IP = 192.168.178.138
 openssl genrsa -out ${WORKER_FQDN}-worker-key.pem 2048
 WORKER_IP=${WORKER_IP} openssl req -new -key ${WORKER_FQDN}-worker-key.pem -out ${WORKER_FQDN}-worker.csr -subj "/CN=${WORKER_FQDN}" -config worker-openssl.cnf
 WORKER_IP=${WORKER_IP} openssl x509 -req -in ${WORKER_FQDN}-worker.csr -CA ca.pem -CAkey ca-key.pem -CAcreateserial -out ${WORKER_FQDN}-worker.pem -days 365 -extensions v3_req -extfile worker-openssl.cnf
 
-# Admin Key
+# Worker 2 Key
+WORKER_FQDN = "worker2"
+WORKER_IP = 192.168.178.139
+openssl genrsa -out ${WORKER_FQDN}-worker-key.pem 2048
+WORKER_IP=${WORKER_IP} openssl req -new -key ${WORKER_FQDN}-worker-key.pem -out ${WORKER_FQDN}-worker.csr -subj "/CN=${WORKER_FQDN}" -config worker-openssl.cnf
+WORKER_IP=${WORKER_IP} openssl x509 -req -in ${WORKER_FQDN}-worker.csr -CA ca.pem -CAkey ca-key.pem -CAcreateserial -out ${WORKER_FQDN}-worker.pem -days 365 -extensions v3_req -extfile worker-openssl.cnf
 
+# Admin Key
 openssl genrsa -out admin-key.pem 2048
 openssl req -new -key admin-key.pem -out admin.csr -subj "/CN=kube-admin"
 openssl x509 -req -in admin.csr -CA ca.pem -CAkey ca-key.pem -CAcreateserial -out admin.pem -days 365
