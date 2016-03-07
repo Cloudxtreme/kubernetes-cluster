b1cee633acf9abc713efd6d79f2f14b04ea11e12
Benjamin Borbe <bborbe@rocketnews.de>
2016-03-07 23:20:42 +0100

    change mode to passthrough

--- scripts/virsh-create.sh
+++ scripts/virsh-create.sh
@@ -15,8 +15,8 @@ virt-install \
 --name node-master \
 --disk /dev/mapper/system-node--master \
 --filesystem /var/lib/libvirt/images/coreos/node-master/config/,config-2,type=mount,mode=squash \
---filesystem /var/lib/libvirt/images/coreos/node-master/etc/kubernetes/,etc-kubernetes,type=mount,mode=squash \
---filesystem /var/lib/libvirt/images/coreos/node-master/srv/kubernetes/,srv-kubernetes,type=mount,mode=squash \
+--filesystem /var/lib/libvirt/images/coreos/node-master/etc/kubernetes/,etc-kubernetes,type=mount,mode=passthrough \
+--filesystem /var/lib/libvirt/images/coreos/node-master/srv/kubernetes/,srv-kubernetes,type=mount,mode=passthrough \
 --network bridge=br0,mac=00:16:3e:1f:3d:a9,type=bridge
 
 echo "create virsh node-worker0 ..."
@@ -34,8 +34,8 @@ virt-install \
 --name node-worker0 \
 --disk /dev/mapper/system-node--worker0 \
 --filesystem /var/lib/libvirt/images/coreos/node-worker0/config/,config-2,type=mount,mode=squash \
---filesystem /var/lib/libvirt/images/coreos/node-worker0/etc/kubernetes/,etc-kubernetes,type=mount,mode=squash \
---filesystem /var/lib/libvirt/images/coreos/node-worker0/srv/kubernetes/,srv-kubernetes,type=mount,mode=squash \
+--filesystem /var/lib/libvirt/images/coreos/node-worker0/etc/kubernetes/,etc-kubernetes,type=mount,mode=passthrough \
+--filesystem /var/lib/libvirt/images/coreos/node-worker0/srv/kubernetes/,srv-kubernetes,type=mount,mode=passthrough \
 --network bridge=br0,mac=00:16:3e:0d:03:6a,type=bridge
 
 echo "create virsh node-worker1 ..."
@@ -53,8 +53,8 @@ virt-install \
 --name node-worker1 \
 --disk /dev/mapper/system-node--worker1 \
 --filesystem /var/lib/libvirt/images/coreos/node-worker1/config/,config-2,type=mount,mode=squash \
---filesystem /var/lib/libvirt/images/coreos/node-worker1/etc/kubernetes/,etc-kubernetes,type=mount,mode=squash \
---filesystem /var/lib/libvirt/images/coreos/node-worker1/srv/kubernetes/,srv-kubernetes,type=mount,mode=squash \
+--filesystem /var/lib/libvirt/images/coreos/node-worker1/etc/kubernetes/,etc-kubernetes,type=mount,mode=passthrough \
+--filesystem /var/lib/libvirt/images/coreos/node-worker1/srv/kubernetes/,srv-kubernetes,type=mount,mode=passthrough \
 --network bridge=br0,mac=00:16:3e:cc:51:23,type=bridge
 
 echo "create virsh node-worker2 ..."
@@ -72,6 +72,6 @@ virt-install \
 --name node-worker2 \
 --disk /dev/mapper/system-node--worker2 \
 --filesystem /var/lib/libvirt/images/coreos/node-worker2/config/,config-2,type=mount,mode=squash \
---filesystem /var/lib/libvirt/images/coreos/node-worker2/etc/kubernetes/,etc-kubernetes,type=mount,mode=squash \
---filesystem /var/lib/libvirt/images/coreos/node-worker2/srv/kubernetes/,srv-kubernetes,type=mount,mode=squash \
+--filesystem /var/lib/libvirt/images/coreos/node-worker2/etc/kubernetes/,etc-kubernetes,type=mount,mode=passthrough \
+--filesystem /var/lib/libvirt/images/coreos/node-worker2/srv/kubernetes/,srv-kubernetes,type=mount,mode=passthrough \
 --network bridge=br0,mac=00:16:3e:9d:5d:d8,type=bridge
