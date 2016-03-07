eaaad89280fd8cd2c7d7c164bb8c2d923a63384e
Benjamin Borbe <bborbe@rocketnews.de>
2016-03-07 21:46:46 +0100

    add etc and srv

--- scripts/create-cluster.sh
+++ scripts/create-cluster.sh
@@ -37,7 +37,7 @@ virt-install \
 --name node-master \
 --disk /dev/mapper/system-node--master \
 --filesystem /var/lib/libvirt/images/coreos/node-master/config/,config-2,type=mount,mode=squash \
---filesystem /var/lib/libvirt/images/coreos/node-master/kubernetes/,/etc/kubernetes,type=mount,mode=squash \
+--filesystem /var/lib/libvirt/images/coreos/node-master/etc/kubernetes/,/etc/kubernetes,type=mount,mode=squash \
 --network bridge=br0,mac=00:16:3e:1f:3d:a9,type=bridge
 
 echo "create virsh node-worker0 ..."
@@ -55,7 +55,8 @@ virt-install \
 --name node-worker0 \
 --disk /dev/mapper/system-node--worker0 \
 --filesystem /var/lib/libvirt/images/coreos/node-worker0/config/,config-2,type=mount,mode=squash \
---filesystem /var/lib/libvirt/images/coreos/node-worker0/kubernetes/,/etc/kubernetes/,type=mount,mode=squash \
+--filesystem /var/lib/libvirt/images/coreos/node-worker0/etc/kubernetes/,/etc/kubernetes/,type=mount,mode=squash \
+--filesystem /var/lib/libvirt/images/coreos/node-worker0/srv/kubernetes/,/srv/kubernetes/,type=mount,mode=squash \
 --network bridge=br0,mac=00:16:3e:0d:03:6a,type=bridge
 
 echo "create virsh node-worker1 ..."
@@ -73,7 +74,8 @@ virt-install \
 --name node-worker1 \
 --disk /dev/mapper/system-node--worker1 \
 --filesystem /var/lib/libvirt/images/coreos/node-worker1/config/,config-2,type=mount,mode=squash \
---filesystem /var/lib/libvirt/images/coreos/node-worker1/kubernetes/,/etc/kubernetes/,type=mount,mode=squash \
+--filesystem /var/lib/libvirt/images/coreos/node-worker1/etc/kubernetes/,/etc/kubernetes/,type=mount,mode=squash \
+--filesystem /var/lib/libvirt/images/coreos/node-worker1/srv/kubernetes/,/srv/kubernetes/,type=mount,mode=squash \
 --network bridge=br0,mac=00:16:3e:cc:51:23,type=bridge
 
 echo "create virsh node-worker2 ..."
@@ -91,7 +93,8 @@ virt-install \
 --name node-worker2 \
 --disk /dev/mapper/system-node--worker2 \
 --filesystem /var/lib/libvirt/images/coreos/node-worker2/config/,config-2,type=mount,mode=squash \
---filesystem /var/lib/libvirt/images/coreos/node-worker2/kubernetes/,/etc/kubernetes/,type=mount,mode=squash \
+--filesystem /var/lib/libvirt/images/coreos/node-worker2/etc/kubernetes/,/etc/kubernetes/,type=mount,mode=squash \
+--filesystem /var/lib/libvirt/images/coreos/node-worker2/srv/kubernetes/,/srv/kubernetes/,type=mount,mode=squash \
 --network bridge=br0,mac=00:16:3e:9d:5d:d8,type=bridge
 
 echo "starting machines ..."
--- ssl/copy_keys.sh
+++ ssl/copy_keys.sh
@@ -1,45 +1,45 @@
 #!/bin/sh
 
 # node-master
-mkdir -p ../node-master/kubernetes/ssl
-cp ca.pem ../node-master/kubernetes/ssl/ca.pem
-cp apiserver.pem ../node-master/kubernetes/ssl/apiserver.pem
-cp apiserver-key.pem ../node-master/kubernetes/ssl/apiserver-key.pem
-chmod 600 ../node-master/kubernetes/ssl/*.pem
-chown root:root ../node-master/kubernetes/ssl/*.pem
+mkdir -p ../node-master/etc/kubernetes/ssl
+cp ca.pem ../node-master/etc/kubernetes/ssl/ca.pem
+cp apiserver.pem ../node-master/etc/kubernetes/ssl/apiserver.pem
+cp apiserver-key.pem ../node-master/etc/kubernetes/ssl/apiserver-key.pem
+chmod 600 ../node-master/etc/kubernetes/ssl/*.pem
+chown root:root ../node-master/etc/kubernetes/ssl/*.pem
 
 # node-worker0
-mkdir -p ../node-worker0/kubernetes/ssl
-cp ca.pem ../node-worker0/kubernetes/ssl/ca.pem
-cp worker0-worker.pem ../node-worker0/kubernetes/ssl/worker0-worker.pem
-cp worker0-worker-key.pem ../node-worker0/kubernetes/ssl/worker0-worker-key.pem
-cd ../node-worker0/kubernetes/ssl
+mkdir -p ../node-worker0/etc/kubernetes/ssl
+cp ca.pem ../node-worker0/etc/kubernetes/ssl/ca.pem
+cp worker0-worker.pem ../node-worker0/etc/kubernetes/ssl/worker0-worker.pem
+cp worker0-worker-key.pem ../node-worker0/etc/kubernetes/ssl/worker0-worker-key.pem
+cd ../node-worker0/etc/kubernetes/ssl
 ln -s worker0-worker.pem worker.pem
 ln -s worker0-worker-key.pem worker-key.pem
 cd -
-chmod 600 ../node-worker0/kubernetes/ssl/*.pem
-chown root:root ../node-worker0/kubernetes/ssl/*.pem
+chmod 600 ../node-worker0/etc/kubernetes/ssl/*.pem
+chown root:root ../node-worker0/etc/kubernetes/ssl/*.pem
 
 # node-worker1
-mkdir -p ../node-worker1/kubernetes/ssl
-cp ca.pem ../node-worker1/kubernetes/ssl/ca.pem
-cp worker1-worker.pem ../node-worker1/kubernetes/ssl/worker1-worker.pem
-cp worker1-worker-key.pem ../node-worker1/kubernetes/ssl/worker1-worker-key.pem
-cd ../node-worker1/kubernetes/ssl
+mkdir -p ../node-worker1/etc/kubernetes/ssl
+cp ca.pem ../node-worker1/etc/kubernetes/ssl/ca.pem
+cp worker1-worker.pem ../node-worker1/etc/kubernetes/ssl/worker1-worker.pem
+cp worker1-worker-key.pem ../node-worker1/etc/kubernetes/ssl/worker1-worker-key.pem
+cd ../node-worker1/etc/kubernetes/ssl
 ln -s worker1-worker.pem worker.pem
 ln -s worker1-worker-key.pem worker-key.pem
 cd -
-chmod 600 ../node-worker1/kubernetes/ssl/*.pem
-chown root:root ../node-worker1/kubernetes/ssl/*.pem
+chmod 600 ../node-worker1/etc/kubernetes/ssl/*.pem
+chown root:root ../node-worker1/etc/kubernetes/ssl/*.pem
 
 # node-worker2
-mkdir -p ../node-worker2/kubernetes/ssl
-cp ca.pem ../node-worker2/kubernetes/ssl/ca.pem
-cp worker2-worker.pem ../node-worker2/kubernetes/ssl/worker2-worker.pem
-cp worker2-worker-key.pem ../node-worker2/kubernetes/ssl/worker2-worker-key.pem
-cd ../node-worker2/kubernetes/ssl
+mkdir -p ../node-worker2/etc/kubernetes/ssl
+cp ca.pem ../node-worker2/etc/kubernetes/ssl/ca.pem
+cp worker2-worker.pem ../node-worker2/etc/kubernetes/ssl/worker2-worker.pem
+cp worker2-worker-key.pem ../node-worker2/etc/kubernetes/ssl/worker2-worker-key.pem
+cd ../node-worker2/etc/kubernetes/ssl
 ln -s worker2-worker.pem worker.pem
 ln -s worker2-worker-key.pem worker-key.pem
 cd -
-chmod 600 ../node-worker2/kubernetes/ssl/*.pem
-chown root:root ../node-worker2/kubernetes/ssl/*.pem
+chmod 600 ../node-worker2/etc/kubernetes/ssl/*.pem
+chown root:root ../node-worker2/etc/kubernetes/ssl/*.pem
