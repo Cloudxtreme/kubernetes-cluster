84f800077ddd6c3b8b9aaf09459fafc683787bb5
Benjamin Borbe <bborbe@rocketnews.de>
2016-03-07 20:49:49 +0100

    fix

--- scripts/destroy-cluster.sh
+++ scripts/destroy-cluster.sh
@@ -3,19 +3,19 @@
 echo "destroying machines ..."
 virsh destroy node-master
 virsh destroy node-worker0
+virsh destroy node-worker1
 virsh destroy node-worker2
-virsh destroy node-worker3
 
 echo "undefine machines ..."
 virsh undefine node-master
 virsh undefine node-worker0
+virsh undefine node-worker1
 virsh undefine node-worker2
-virsh undefine node-worker3
 
 echo "remove lvm volumes ..."
 lvremove /dev/system/node-master
 lvremove /dev/system/node-worker0
-lvremove /dev/system/node-worker2
+lvremove /dev/system/node-worker1
 lvremove /dev/system/node-worker2
 
 echo "done"
