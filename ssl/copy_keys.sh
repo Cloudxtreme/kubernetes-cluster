ec732fdfe57fbd0f332d0b9e0fe89a7e35d5e479
Benjamin Borbe <bborbe@rocketnews.de>
2016-03-08 14:37:42 +0100

    remove /etc/kubernetes and /srv/kubernetes mount

--- ssl/copy_keys.sh
+++ ssl/copy_keys.sh
@@ -1,45 +1,33 @@
 #!/bin/sh
 
 # node-master
-mkdir -p ../node-master/etc/kubernetes/ssl
-cp ca.pem ../node-master/etc/kubernetes/ssl/ca.pem
-cp apiserver.pem ../node-master/etc/kubernetes/ssl/apiserver.pem
-cp apiserver-key.pem ../node-master/etc/kubernetes/ssl/apiserver-key.pem
-#chmod 600 ../node-master/etc/kubernetes/ssl/*.pem
-chown root:root ../node-master/etc/kubernetes/ssl/*.pem
+mkdir -p ../node-master/ssl
+cp ca.pem ../node-master/ssl/ca.pem
+cp apiserver.pem ../node-master/ssl/apiserver.pem
+cp apiserver-key.pem ../node-master/ssl/apiserver-key.pem
+#chmod 600 ../node-master/ssl/*.pem
+chown root:root ../node-master/ssl/*.pem
 
 # node-worker0
-mkdir -p ../node-worker0/etc/kubernetes/ssl
-cp ca.pem ../node-worker0/etc/kubernetes/ssl/ca.pem
-cp worker0-worker.pem ../node-worker0/etc/kubernetes/ssl/worker0-worker.pem
-cp worker0-worker-key.pem ../node-worker0/etc/kubernetes/ssl/worker0-worker-key.pem
-cd ../node-worker0/etc/kubernetes/ssl
-ln -s worker0-worker.pem worker.pem
-ln -s worker0-worker-key.pem worker-key.pem
-cd -
-#chmod 600 ../node-worker0/etc/kubernetes/ssl/*.pem
-chown root:root ../node-worker0/etc/kubernetes/ssl/*.pem
+mkdir -p ../node-worker0/ssl
+cp ca.pem ../node-worker0/ssl/ca.pem
+cp worker0-worker.pem ../node-worker0/ssl/worker.pem
+cp worker0-worker-key.pem ../node-worker0/ssl/worker-key.pem
+#chmod 600 ../node-worker0/ssl/*.pem
+chown root:root ../node-worker0/ssl/*.pem
 
 # node-worker1
-mkdir -p ../node-worker1/etc/kubernetes/ssl
-cp ca.pem ../node-worker1/etc/kubernetes/ssl/ca.pem
-cp worker1-worker.pem ../node-worker1/etc/kubernetes/ssl/worker1-worker.pem
-cp worker1-worker-key.pem ../node-worker1/etc/kubernetes/ssl/worker1-worker-key.pem
-cd ../node-worker1/etc/kubernetes/ssl
-ln -s worker1-worker.pem worker.pem
-ln -s worker1-worker-key.pem worker-key.pem
-cd -
-#chmod 600 ../node-worker1/etc/kubernetes/ssl/*.pem
-chown root:root ../node-worker1/etc/kubernetes/ssl/*.pem
+mkdir -p ../node-worker1/ssl
+cp ca.pem ../node-worker1/ssl/ca.pem
+cp worker1-worker.pem ../node-worker1/ssl/worker.pem
+cp worker1-worker-key.pem ../node-worker1/ssl/worker-key.pem
+#chmod 600 ../node-worker1/ssl/*.pem
+chown root:root ../node-worker1/ssl/*.pem
 
 # node-worker2
-mkdir -p ../node-worker2/etc/kubernetes/ssl
-cp ca.pem ../node-worker2/etc/kubernetes/ssl/ca.pem
-cp worker2-worker.pem ../node-worker2/etc/kubernetes/ssl/worker2-worker.pem
-cp worker2-worker-key.pem ../node-worker2/etc/kubernetes/ssl/worker2-worker-key.pem
-cd ../node-worker2/etc/kubernetes/ssl
-ln -s worker2-worker.pem worker.pem
-ln -s worker2-worker-key.pem worker-key.pem
-cd -
-#chmod 600 ../node-worker2/etc/kubernetes/ssl/*.pem
-chown root:root ../node-worker2/etc/kubernetes/ssl/*.pem
+mkdir -p ../node-worker2/ssl
+cp ca.pem ../node-worker2/ssl/ca.pem
+cp worker2-worker.pem ../node-worker2/ssl/worker.pem
+cp worker2-worker-key.pem ../node-worker2/ssl/worker-key.pem
+#chmod 600 ../node-worker2/ssl/*.pem
+chown root:root ../node-worker2/ssl/*.pem
