3ffce926f57e8e3edc1916e21ce4ce3b74002ddc
Benjamin Borbe <bborbe@rocketnews.de>
2016-03-09 09:33:16 +0100

    rename ssl keys

--- scripts/ssl-copy-keys.sh
+++ scripts/ssl-copy-keys.sh
@@ -11,23 +11,23 @@ chown root:root ../kubernetes-master/ssl/*.pem
 # kubernetes-worker0
 mkdir -p ../kubernetes-worker0/ssl
 cp kubernetes-ca.pem ../kubernetes-worker0/ssl/ca.pem
-cp kubernetes-worker0.pm ../kubernetes-worker0/ssl/worker.pem
-cp kubernetes-worker0.pm ../kubernetes-worker0/ssl/worker-key.pem
+cp kubernetes-worker0.pem ../kubernetes-worker0/ssl/worker.pem
+cp kubernetes-worker0.pem ../kubernetes-worker0/ssl/worker-key.pem
 #chmod 600 ../kubernetes-worker0/ssl/*.pem
 chown root:root ../kubernetes-worker0/ssl/*.pem
 
 # kubernetes-worker1
 mkdir -p ../kubernetes-worker1/ssl
 cp kubernetes-ca.pem ../kubernetes-worker1/ssl/ca.pem
-cp kubernetes-worker1.pm ../kubernetes-worker1/ssl/worker.pem
-cp kubernetes-worker1.pm ../kubernetes-worker1/ssl/worker-key.pem
+cp kubernetes-worker1.pem ../kubernetes-worker1/ssl/worker.pem
+cp kubernetes-worker1.pem ../kubernetes-worker1/ssl/worker-key.pem
 #chmod 600 ../kubernetes-worker1/ssl/*.pem
 chown root:root ../kubernetes-worker1/ssl/*.pem
 
 # kubernetes-worker2
 mkdir -p ../kubernetes-worker2/ssl
 cp kubernetes-ca.pem ../kubernetes-worker2/ssl/ca.pem
-cp kubernetes-worker2.pm ../kubernetes-worker2/ssl/worker.pem
-cp kubernetes-worker2.pm ../kubernetes-worker2/ssl/worker-key.pem
+cp kubernetes-worker2.pem ../kubernetes-worker2/ssl/worker.pem
+cp kubernetes-worker2.pem ../kubernetes-worker2/ssl/worker-key.pem
 #chmod 600 ../kubernetes-worker2/ssl/*.pem
 chown root:root ../kubernetes-worker2/ssl/*.pem
