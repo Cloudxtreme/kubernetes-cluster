708eaff89fa46ac0409a8fc0c3d25835095eea16
Benjamin Borbe <bborbe@rocketnews.de>
2016-03-09 08:53:02 +0100

    rename ip's

--- ssl/generate_keys.sh
+++ ssl/generate_keys.sh
@@ -13,21 +13,21 @@ openssl x509 -req -in apiserver.csr -CA ca.pem -CAkey ca-key.pem -CAcreateserial
 
 # Worker 0 Key
 WORKER_FQDN="worker0"
-WORKER_IP=192.168.178.140
+WORKER_IP=172.16.20.0.20
 openssl genrsa -out ${WORKER_FQDN}-worker-key.pem 2048
 WORKER_IP=${WORKER_IP} openssl req -new -key ${WORKER_FQDN}-worker-key.pem -out ${WORKER_FQDN}-worker.csr -subj "/CN=${WORKER_FQDN}" -config worker-openssl.cnf
 WORKER_IP=${WORKER_IP} openssl x509 -req -in ${WORKER_FQDN}-worker.csr -CA ca.pem -CAkey ca-key.pem -CAcreateserial -out ${WORKER_FQDN}-worker.pem -days 365 -extensions v3_req -extfile worker-openssl.cnf
 
 # Worker 1 Key
 WORKER_FQDN="worker1"
-WORKER_IP=192.168.178.141
+WORKER_IP=172.16.20.0.21
 openssl genrsa -out ${WORKER_FQDN}-worker-key.pem 2048
 WORKER_IP=${WORKER_IP} openssl req -new -key ${WORKER_FQDN}-worker-key.pem -out ${WORKER_FQDN}-worker.csr -subj "/CN=${WORKER_FQDN}" -config worker-openssl.cnf
 WORKER_IP=${WORKER_IP} openssl x509 -req -in ${WORKER_FQDN}-worker.csr -CA ca.pem -CAkey ca-key.pem -CAcreateserial -out ${WORKER_FQDN}-worker.pem -days 365 -extensions v3_req -extfile worker-openssl.cnf
 
 # Worker 2 Key
 WORKER_FQDN="worker2"
-WORKER_IP=192.168.178.142
+WORKER_IP=172.16.20.0.22
 openssl genrsa -out ${WORKER_FQDN}-worker-key.pem 2048
 WORKER_IP=${WORKER_IP} openssl req -new -key ${WORKER_FQDN}-worker-key.pem -out ${WORKER_FQDN}-worker.csr -subj "/CN=${WORKER_FQDN}" -config worker-openssl.cnf
 WORKER_IP=${WORKER_IP} openssl x509 -req -in ${WORKER_FQDN}-worker.csr -CA ca.pem -CAkey ca-key.pem -CAcreateserial -out ${WORKER_FQDN}-worker.pem -days 365 -extensions v3_req -extfile worker-openssl.cnf
