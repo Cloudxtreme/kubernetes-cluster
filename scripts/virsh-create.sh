507e75cf6e68e3ff16deb540bf2dc8bf7a1eb3d1
Benjamin Borbe <bborbe@rocketnews.de>
2016-03-09 09:34:23 +0100

    fix device names

--- scripts/virsh-create.sh
+++ scripts/virsh-create.sh
@@ -13,7 +13,7 @@ virt-install \
 --noautoconsole \
 --nographics \
 --name kubernetes-master \
---disk /dev/mapper/system-node--master \
+--disk /dev/system/kubernetes-master \
 --filesystem /var/lib/libvirt/images/coreos/kubernetes-master/config/,config-2,type=mount,mode=squash \
 --filesystem /var/lib/libvirt/images/coreos/kubernetes-master/ssl/,kubernetes-ssl,type=mount,mode=squash \
 --network bridge=br0,mac=00:16:3e:1f:3d:a9,type=bridge
@@ -31,7 +31,7 @@ virt-install \
 --noautoconsole \
 --nographics \
 --name kubernetes-worker0 \
---disk /dev/mapper/system-node--worker0 \
+--disk /dev/system/kubernetes-worker0 \
 --filesystem /var/lib/libvirt/images/coreos/kubernetes-worker0/config/,config-2,type=mount,mode=squash \
 --filesystem /var/lib/libvirt/images/coreos/kubernetes-worker0/ssl/,kubernetes-ssl,type=mount,mode=squash \
 --network bridge=br0,mac=00:16:3e:0d:03:6a,type=bridge
@@ -49,7 +49,7 @@ virt-install \
 --noautoconsole \
 --nographics \
 --name kubernetes-worker1 \
---disk /dev/mapper/system-node--worker1 \
+--disk /dev/system/kubernetes-worker1 \
 --filesystem /var/lib/libvirt/images/coreos/kubernetes-worker1/config/,config-2,type=mount,mode=squash \
 --filesystem /var/lib/libvirt/images/coreos/kubernetes-worker1/ssl/,kubernetes-ssl,type=mount,mode=squash \
 --network bridge=br0,mac=00:16:3e:cc:51:23,type=bridge
@@ -67,7 +67,7 @@ virt-install \
 --noautoconsole \
 --nographics \
 --name kubernetes-worker2 \
---disk /dev/mapper/system-node--worker2 \
+--disk /dev/system/kubernetes-worker2 \
 --filesystem /var/lib/libvirt/images/coreos/kubernetes-worker2/config/,config-2,type=mount,mode=squash \
 --filesystem /var/lib/libvirt/images/coreos/kubernetes-worker2/ssl/,kubernetes-ssl,type=mount,mode=squash \
 --network bridge=br0,mac=00:16:3e:9d:5d:d8,type=bridge
