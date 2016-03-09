4765d430fb5cbe6f327c2b803a358633f39739b3
Benjamin Borbe <bborbe@rocketnews.de>
2016-03-09 09:48:00 +0100

    update set kubectl

--- scripts/configure-kubectl.sh
+++ scripts/configure-kubectl.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-kubectl config set-cluster default-cluster --server=https://172.16.20.10 --certificate-authority=/Users/bborbe/.kubectl/ca.pem
-kubectl config set-credentials default-admin --certificate-authority=/Users/bborbe/.kubectl/ca.pem --client-key=/Users/bborbe/.kubectl/admin-key.pem --client-certificate=/Users/bborbe/.kubectl/admin.pem
+kubectl config set-cluster default-cluster --server=https://172.16.20.10 --certificate-authority=/Users/bborbe/.kubectl/kubernetes-ca.pem
+kubectl config set-credentials default-admin --certificate-authority=/Users/bborbe/.kubectl/kubernetes-ca.pem --client-key=/Users/bborbe/.kubectl/kubernetes-admin-key.pem --client-certificate=/Users/bborbe/.kubectl/kubernetes-admin.pem
 kubectl config set-context default-system --cluster=default-cluster --user=default-admin
 kubectl config use-context default-system
 
