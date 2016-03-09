a21cf4c72e397bd958e086997fccffd68520184e
Benjamin Borbe <bborbe@rocketnews.de>
2016-03-09 09:43:44 +0100

    fix ips

--- scripts/configure-kubectl.sh
+++ scripts/configure-kubectl.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-kubectl config set-cluster default-cluster --server=https://172.16.20.0.10 --certificate-authority=/Users/bborbe/.kubectl/ca.pem
+kubectl config set-cluster default-cluster --server=https://172.16.20.10 --certificate-authority=/Users/bborbe/.kubectl/ca.pem
 kubectl config set-credentials default-admin --certificate-authority=/Users/bborbe/.kubectl/ca.pem --client-key=/Users/bborbe/.kubectl/admin-key.pem --client-certificate=/Users/bborbe/.kubectl/admin.pem
 kubectl config set-context default-system --cluster=default-cluster --user=default-admin
 kubectl config use-context default-system
