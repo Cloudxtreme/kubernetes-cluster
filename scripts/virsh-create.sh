253f48d56666e8880b8aed458ee51be0dcea2e7e
Benjamin Borbe <bborbe@rocketnews.de>
2016-03-09 09:35:21 +0100

    fix mount

--- scripts/virsh-create.sh
+++ scripts/virsh-create.sh
@@ -14,8 +14,8 @@ virt-install \
 --nographics \
 --name kubernetes-master \
 --disk /dev/system/kubernetes-master \
---filesystem /var/lib/libvirt/images/coreos/kubernetes-master/config/,config-2,type=mount,mode=squash \
---filesystem /var/lib/libvirt/images/coreos/kubernetes-master/ssl/,kubernetes-ssl,type=mount,mode=squash \
+--filesystem /var/lib/libvirt/images/kubernetes/kubernetes-master/config/,config-2,type=mount,mode=squash \
+--filesystem /var/lib/libvirt/images/kubernetes/kubernetes-master/ssl/,kubernetes-ssl,type=mount,mode=squash \
 --network bridge=br0,mac=00:16:3e:1f:3d:a9,type=bridge
 
 echo "create virsh kubernetes-worker0 ..."
@@ -32,8 +32,8 @@ virt-install \
 --nographics \
 --name kubernetes-worker0 \
 --disk /dev/system/kubernetes-worker0 \
---filesystem /var/lib/libvirt/images/coreos/kubernetes-worker0/config/,config-2,type=mount,mode=squash \
---filesystem /var/lib/libvirt/images/coreos/kubernetes-worker0/ssl/,kubernetes-ssl,type=mount,mode=squash \
+--filesystem /var/lib/libvirt/images/kubernetes/kubernetes-worker0/config/,config-2,type=mount,mode=squash \
+--filesystem /var/lib/libvirt/images/kubernetes/kubernetes-worker0/ssl/,kubernetes-ssl,type=mount,mode=squash \
 --network bridge=br0,mac=00:16:3e:0d:03:6a,type=bridge
 
 echo "create virsh kubernetes-worker1 ..."
@@ -50,8 +50,8 @@ virt-install \
 --nographics \
 --name kubernetes-worker1 \
 --disk /dev/system/kubernetes-worker1 \
---filesystem /var/lib/libvirt/images/coreos/kubernetes-worker1/config/,config-2,type=mount,mode=squash \
---filesystem /var/lib/libvirt/images/coreos/kubernetes-worker1/ssl/,kubernetes-ssl,type=mount,mode=squash \
+--filesystem /var/lib/libvirt/images/kubernetes/kubernetes-worker1/config/,config-2,type=mount,mode=squash \
+--filesystem /var/lib/libvirt/images/kubernetes/kubernetes-worker1/ssl/,kubernetes-ssl,type=mount,mode=squash \
 --network bridge=br0,mac=00:16:3e:cc:51:23,type=bridge
 
 echo "create virsh kubernetes-worker2 ..."
@@ -68,6 +68,6 @@ virt-install \
 --nographics \
 --name kubernetes-worker2 \
 --disk /dev/system/kubernetes-worker2 \
---filesystem /var/lib/libvirt/images/coreos/kubernetes-worker2/config/,config-2,type=mount,mode=squash \
---filesystem /var/lib/libvirt/images/coreos/kubernetes-worker2/ssl/,kubernetes-ssl,type=mount,mode=squash \
+--filesystem /var/lib/libvirt/images/kubernetes/kubernetes-worker2/config/,config-2,type=mount,mode=squash \
+--filesystem /var/lib/libvirt/images/kubernetes/kubernetes-worker2/ssl/,kubernetes-ssl,type=mount,mode=squash \
 --network bridge=br0,mac=00:16:3e:9d:5d:d8,type=bridge
