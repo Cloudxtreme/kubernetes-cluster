ebe1997c9a777e4500da878bc6074ee9a7d35f90
Benjamin Borbe <bborbe@rocketnews.de>
2016-03-09 09:57:59 +0100

    use private bridge

--- scripts/virsh-create.sh
+++ scripts/virsh-create.sh
@@ -16,7 +16,7 @@ virt-install \
 --disk /dev/system/kubernetes-master \
 --filesystem /var/lib/libvirt/images/kubernetes/kubernetes-master/config/,config-2,type=mount,mode=squash \
 --filesystem /var/lib/libvirt/images/kubernetes/kubernetes-master/ssl/,kubernetes-ssl,type=mount,mode=squash \
---network bridge=br0,mac=00:16:3e:1f:3d:a9,type=bridge
+--network bridge=privatebr0,mac=00:16:3e:1f:3d:a9,type=bridge
 
 echo "create virsh kubernetes-worker0 ..."
 virt-install \
@@ -34,7 +34,7 @@ virt-install \
 --disk /dev/system/kubernetes-worker0 \
 --filesystem /var/lib/libvirt/images/kubernetes/kubernetes-worker0/config/,config-2,type=mount,mode=squash \
 --filesystem /var/lib/libvirt/images/kubernetes/kubernetes-worker0/ssl/,kubernetes-ssl,type=mount,mode=squash \
---network bridge=br0,mac=00:16:3e:0d:03:6a,type=bridge
+--network bridge=privatebr0,mac=00:16:3e:0d:03:6a,type=bridge
 
 echo "create virsh kubernetes-worker1 ..."
 virt-install \
@@ -52,7 +52,7 @@ virt-install \
 --disk /dev/system/kubernetes-worker1 \
 --filesystem /var/lib/libvirt/images/kubernetes/kubernetes-worker1/config/,config-2,type=mount,mode=squash \
 --filesystem /var/lib/libvirt/images/kubernetes/kubernetes-worker1/ssl/,kubernetes-ssl,type=mount,mode=squash \
---network bridge=br0,mac=00:16:3e:cc:51:23,type=bridge
+--network bridge=privatebr0,mac=00:16:3e:cc:51:23,type=bridge
 
 echo "create virsh kubernetes-worker2 ..."
 virt-install \
@@ -70,4 +70,4 @@ virt-install \
 --disk /dev/system/kubernetes-worker2 \
 --filesystem /var/lib/libvirt/images/kubernetes/kubernetes-worker2/config/,config-2,type=mount,mode=squash \
 --filesystem /var/lib/libvirt/images/kubernetes/kubernetes-worker2/ssl/,kubernetes-ssl,type=mount,mode=squash \
---network bridge=br0,mac=00:16:3e:9d:5d:d8,type=bridge
+--network bridge=privatebr0,mac=00:16:3e:9d:5d:d8,type=bridge
