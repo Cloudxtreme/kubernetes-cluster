b00f3642d106cb2abce01632764cf9d91d0d61ea
Benjamin Borbe <bborbe@rocketnews.de>
2016-03-22 21:46:03 +0100

    update gen

--- utils/config-generator.sh
+++ utils/config-generator.sh
@@ -1,31 +1,41 @@
 #!/bin/bash
 
-WORKER_AMOUNT=3
-
 function generate_configs {
 	echo "generate_configs started"
-	create_directories
+	create_nodes
 	echo "generate_configs finished"
 }
 
-function create_directories {
-	echo "create_directories started"
-	create_ssl "kubernetes-master"
-	copy_user_data "kubernetes-master"
-	create_ssl "kubernetes-storage"
-	copy_user_data "kubernetes-storage"
+function generate_mac {
+	printf '00:16:3e:2f:06:%x' $1
+}
+
+function create_nodes {
+	echo "create_nodes started"
+	create_node "kubernetes-master" "kubernetes-master" "10"
+	create_node "kubernetes-storage" "kubernetes-storage" "9"
 	for ((i=0; i < WORKER_AMOUNT; i++)) do
-		create_ssl "kubernetes-worker$i"
-		copy_user_data "kubernetes-worker$i"
+		value=$((20 + $i))
+		create_node "kubernetes-worker" "kubernetes-worker$i" "$value"
 	done
-	echo "create_directories finished"
+	echo "create_nodes finished"
+}
+
+function create_node {
+	echo "create_nodes $2 started"
+	NODENAME=$2
+	NODEIP="$NETWORK.$3"
+	NODEMAC=$(generate_mac "$3")
+	create_ssl $2
+	copy_user_data $1 $2
+	echo "create_nodes $2 finished"
 }
 
 function copy_user_data {
-	echo "copy_user_data for $1 started"
+	echo "copy_user_data for $2 started"
 	mkdir -p $1/config/openstack/latest
-	render template/$1/config/openstack/latest/user_data > $1/config/openstack/latest/user_data
-	echo "copy_user_data for $1 finished"
+	render template/$1/config/openstack/latest/user_data > $2/config/openstack/latest/user_data
+	echo "copy_user_data for $2 finished"
 }
 
 function create_ssl {
