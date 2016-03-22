bfc385682697b8d1ea68a7ce0377af10fea12a95
Benjamin Borbe <bborbe@rocketnews.de>
2016-03-22 23:44:50 +0100

    move image

--- scripts/cluster-create.sh
+++ scripts/cluster-create.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-if [ ! -f /mnt/images/coreos/coreos_production_qemu_image.img ]; then
+if [ ! -f /var/lib/libvirt/images/coreos_production_qemu_image.img ]; then
   echo "downloading image ..."
-  #wget http://stable.release.core-os.net/amd64-usr/current/coreos_production_qemu_image.img.bz2 -O - | bzcat > /mnt/images/coreos/coreos_production_qemu_image.img
-  wget http://beta.release.core-os.net/amd64-usr/current/coreos_production_qemu_image.img.bz2 -O - | bzcat > /mnt/images/coreos/coreos_production_qemu_image.img
-  #wget http://alpha.release.core-os.net/amd64-usr/current/coreos_production_qemu_image.img.bz2 -O - | bzcat > /mnt/images/coreos/coreos_production_qemu_image.img
+  #wget http://stable.release.core-os.net/amd64-usr/current/coreos_production_qemu_image.img.bz2 -O - | bzcat > /var/lib/libvirt/images/coreos_production_qemu_image.img
+  wget http://beta.release.core-os.net/amd64-usr/current/coreos_production_qemu_image.img.bz2 -O - | bzcat > /var/lib/libvirt/images/coreos_production_qemu_image.img
+  #wget http://alpha.release.core-os.net/amd64-usr/current/coreos_production_qemu_image.img.bz2 -O - | bzcat > /var/lib/libvirt/images/coreos_production_qemu_image.img
 fi
 
 echo "converting image ..."
-qemu-img convert /mnt/images/coreos/coreos_production_qemu_image.img -O raw /mnt/images/coreos/coreos_production_qemu_image.raw
+qemu-img convert /var/lib/libvirt/images/coreos_production_qemu_image.img -O raw /var/lib/libvirt/images/coreos_production_qemu_image.raw
 
 echo "create lvm volumes ..."
 lvcreate -L 10G -n bb_kubernetes-master vg0
@@ -18,11 +18,11 @@ lvcreate -L 10G -n bb_kubernetes-worker1 vg0
 lvcreate -L 10G -n bb_kubernetes-worker2 vg0
 
 echo "writing images ..."
-dd bs=1M iflag=direct oflag=direct if=/mnt/images/coreos/coreos_production_qemu_image.raw of=/dev/vg0/bb_kubernetes-master
-dd bs=1M iflag=direct oflag=direct if=/mnt/images/coreos/coreos_production_qemu_image.raw of=/dev/vg0/bb_kubernetes-storage
-dd bs=1M iflag=direct oflag=direct if=/mnt/images/coreos/coreos_production_qemu_image.raw of=/dev/vg0/bb_kubernetes-worker0
-dd bs=1M iflag=direct oflag=direct if=/mnt/images/coreos/coreos_production_qemu_image.raw of=/dev/vg0/bb_kubernetes-worker1
-dd bs=1M iflag=direct oflag=direct if=/mnt/images/coreos/coreos_production_qemu_image.raw of=/dev/vg0/bb_kubernetes-worker2
+dd bs=1M iflag=direct oflag=direct if=/var/lib/libvirt/images/coreos_production_qemu_image.raw of=/dev/vg0/bb_kubernetes-master
+dd bs=1M iflag=direct oflag=direct if=/var/lib/libvirt/images/coreos_production_qemu_image.raw of=/dev/vg0/bb_kubernetes-storage
+dd bs=1M iflag=direct oflag=direct if=/var/lib/libvirt/images/coreos_production_qemu_image.raw of=/dev/vg0/bb_kubernetes-worker0
+dd bs=1M iflag=direct oflag=direct if=/var/lib/libvirt/images/coreos_production_qemu_image.raw of=/dev/vg0/bb_kubernetes-worker1
+dd bs=1M iflag=direct oflag=direct if=/var/lib/libvirt/images/coreos_production_qemu_image.raw of=/dev/vg0/bb_kubernetes-worker2
 
 ./virsh-create.sh
 
