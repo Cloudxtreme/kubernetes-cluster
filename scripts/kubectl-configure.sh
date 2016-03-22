fcf0fcd1f41c2bb828d3cd121bb8cffa6d26ecf5
Benjamin Borbe <bborbe@rocketnews.de>
2016-03-22 22:46:58 +0100

    update apiserver ip

--- scripts/kubectl-configure.sh
+++ scripts/kubectl-configure.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 mkdir -p $HOME/.kube/rn
-kubectl config set-cluster rn-cluster --server=https://192.168.178.3 --certificate-authority=$HOME/.kube/rn/kubernetes-ca.pem
+kubectl config set-cluster rn-cluster --server=https://172.16.50.1 --certificate-authority=$HOME/.kube/rn/kubernetes-ca.pem
 kubectl config set-credentials rn-admin --certificate-authority=$HOME/.kube/rn/kubernetes-ca.pem --client-key=$HOME/.kube/rn/kubernetes-admin-key.pem --client-certificate=$HOME/.kube/rn/kubernetes-admin.pem
 kubectl config set-context rn-system --cluster=rn-cluster --user=rn-admin
 kubectl config use-context rn-system
