b32193bacc8d79a0057a9cf78d2a49b6109a6a44
Benjamin Borbe <bborbe@rocketnews.de>
2016-03-22 22:51:19 +0100

    update mac

--- generate-rn-config.sh
+++ generate-rn-config.sh
@@ -6,13 +6,12 @@ set -o pipefail
 
 ################################################################################
 
-
 API_SERVER="172.16.50.1"
 CLUSTER_NAME="cluster1"
 REGION="rn"
 WORKER_AMOUNT=3
 NETWORK="172.16.30"
-MACPREFIX="00:16:3e:2f:06:"
+MACPREFIX="00:16:3e:2f:30:"
 LVM_VG="vg0"
 PARTITION_PREFIX="bb_"
 VM_PREFIX="bb_"
