ced5482c453b675e2c486342c6ae30d6130433b3
Benjamin Borbe <bborbe@rocketnews.de>
2016-03-22 22:54:53 +0100

    rename

--- generate-hm-config.sh
+++ generate-hm-config.sh
@@ -7,8 +7,8 @@ set -o pipefail
 ################################################################################
 
 API_SERVER="172.16.60.3"
-CLUSTER_NAME="cluster-pn"
-REGION="rn"
+CLUSTER_NAME="cluster-hm"
+REGION="hm"
 WORKER_AMOUNT=3
 NETWORK="172.16.20"
 MACPREFIX="00:16:3e:2f:20:"
