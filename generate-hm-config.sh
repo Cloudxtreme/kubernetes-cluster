32369c3dd52754a236bd03b03612795ca32e7626
Benjamin Borbe <bborbe@rocketnews.de>
2016-03-22 22:51:00 +0100

    configure pn + hm

--- generate-hm-config.sh
+++ generate-hm-config.sh
@@ -1,2 +1,23 @@
 #!/bin/bash
 
+set -o errexit
+set -o nounset
+set -o pipefail
+
+################################################################################
+
+API_SERVER="172.16.60.3"
+CLUSTER_NAME="cluster-pn"
+REGION="rn"
+WORKER_AMOUNT=3
+NETWORK="172.16.20"
+MACPREFIX="00:16:3e:2f:20:"
+LVM_VG="system"
+PARTITION_PREFIX=""
+VM_PREFIX=""
+
+################################################################################
+
+source "utils/config-generator.sh"
+
+generate_configs
--- generate-pn-config.sh
+++ generate-pn-config.sh
@@ -1,2 +1,23 @@
 #!/bin/bash
 
+set -o errexit
+set -o nounset
+set -o pipefail
+
+################################################################################
+
+API_SERVER="172.16.90.1"
+CLUSTER_NAME="cluster-pn"
+REGION="rn"
+WORKER_AMOUNT=3
+NETWORK="172.16.90"
+MACPREFIX="00:16:3e:2f:90:"
+LVM_VG="system"
+PARTITION_PREFIX=""
+VM_PREFIX=""
+
+################################################################################
+
+source "utils/config-generator.sh"
+
+generate_configs
