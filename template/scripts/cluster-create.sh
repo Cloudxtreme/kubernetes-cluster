a15f19db47060ec8eb5f632c707f8357037a57f8
Benjamin Borbe <bborbe@rocketnews.de>
2016-03-26 00:40:53 +0100

    add partition size

--- template/scripts/cluster-create.sh
+++ template/scripts/cluster-create.sh
@@ -11,11 +11,11 @@ echo \"converting image ...\"
 qemu-img convert /var/lib/libvirt/images/coreos_production_qemu_image.img -O raw /var/lib/libvirt/images/coreos_production_qemu_image.raw
 
 echo \"create lvm volumes ...\"
-lvcreate -L 10G -n ${VM_PREFIX}kubernetes-master ${LVM_VG}
-lvcreate -L 10G -n ${VM_PREFIX}kubernetes-storage ${LVM_VG}
-lvcreate -L 10G -n ${VM_PREFIX}kubernetes-worker0 ${LVM_VG}
-lvcreate -L 10G -n ${VM_PREFIX}kubernetes-worker1 ${LVM_VG}
-lvcreate -L 10G -n ${VM_PREFIX}kubernetes-worker2 ${LVM_VG}
+lvcreate -L ${PARTITION_SIZE} -n ${VM_PREFIX}kubernetes-master ${LVM_VG}
+lvcreate -L ${PARTITION_SIZE} -n ${VM_PREFIX}kubernetes-storage ${LVM_VG}
+lvcreate -L ${PARTITION_SIZE} -n ${VM_PREFIX}kubernetes-worker0 ${LVM_VG}
+lvcreate -L ${PARTITION_SIZE} -n ${VM_PREFIX}kubernetes-worker1 ${LVM_VG}
+lvcreate -L ${PARTITION_SIZE} -n ${VM_PREFIX}kubernetes-worker2 ${LVM_VG}
 
 echo \"writing images ...\"
 dd bs=1M iflag=direct oflag=direct if=/var/lib/libvirt/images/coreos_production_qemu_image.raw of=/dev/${LVM_VG}/${PARTITION_PREFIX}kubernetes-master
