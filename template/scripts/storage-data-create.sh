fe148c649f2793454b34ddf23ae6efa1f0ace826
Benjamin Borbe <bborbe@rocketnews.de>
2016-03-26 19:02:12 +0100

    aus ext4

--- template/scripts/storage-data-create.sh
+++ template/scripts/storage-data-create.sh
@@ -5,4 +5,5 @@ lvcreate -L ${STORAGE_SIZE} -n ${VM_PREFIX}kubernetes-storage-data ${LVM_VG}
 
 echo \"format data volum ...\"
 wipefs /dev/${LVM_VG}/${PARTITION_PREFIX}kubernetes-storage-data
-mkfs.btrfs /dev/${LVM_VG}/${PARTITION_PREFIX}kubernetes-storage-data
+mkfs.ext4 -F /dev/${LVM_VG}/${PARTITION_PREFIX}kubernetes-storage-data
+#mkfs.btrfs /dev/${LVM_VG}/${PARTITION_PREFIX}kubernetes-storage-data
