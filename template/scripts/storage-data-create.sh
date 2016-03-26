6db9cea7ac4c097eb21e604f043613bf350d63f9
Benjamin Borbe <bborbe@rocketnews.de>
2016-03-26 16:10:27 +0100

    use btrfs for storage

--- template/scripts/storage-data-create.sh
+++ template/scripts/storage-data-create.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo \"create lvm data volumes ...\"
-lvcreate -L 10G -n ${VM_PREFIX}kubernetes-storage-data ${LVM_VG}
+lvcreate -L ${STORAGE_SIZE} -n ${VM_PREFIX}kubernetes-storage-data ${LVM_VG}
 
 echo \"format data volum ...\"
 wipefs /dev/${LVM_VG}/${PARTITION_PREFIX}kubernetes-storage-data
-mkfs.ext4 -F /dev/${LVM_VG}/${PARTITION_PREFIX}kubernetes-storage-data
+mkfs.btrfs /dev/${LVM_VG}/${PARTITION_PREFIX}kubernetes-storage-data
