d927ced5f2777078bfb055de39e97a352b4dca9d
Benjamin Borbe <bborbe@rocketnews.de>
2016-03-30 16:34:38 +0200

    use beta

--- template/scripts/cluster-create.sh
+++ template/scripts/cluster-create.sh
@@ -3,7 +3,9 @@
 SCRIPT_ROOT=\$(dirname "\${BASH_SOURCE}")
 
 echo \"downloading image ...\"
-wget http://stable.release.core-os.net/amd64-usr/current/coreos_production_qemu_image.img.bz2 -O - | bzcat > /var/lib/libvirt/images/coreos_production_qemu_image.img
+#wget http://stable.release.core-os.net/amd64-usr/current/coreos_production_qemu_image.img.bz2 -O - | bzcat > /var/lib/libvirt/images/coreos_production_qemu_image.img
+wget http://beta.release.core-os.net/amd64-usr/current/coreos_production_qemu_image.img.bz2 -O - | bzcat > /var/lib/libvirt/images/coreos_production_qemu_image.img
+#wget http://alpha.release.core-os.net/amd64-usr/current/coreos_production_qemu_image.img.bz2 -O - | bzcat > /var/lib/libvirt/images/coreos_production_qemu_image.img
 
 echo \"converting image ...\"
 qemu-img convert /var/lib/libvirt/images/coreos_production_qemu_image.img -O raw /var/lib/libvirt/images/coreos_production_qemu_image.raw
